e42a2c86960bf2b4884c25421def49423479f814
pepe-le-pewploo <mamun.muntasir003@gmail.com>
2024-12-15 01:45:19 +0600

    Used axios library to call OperWeatherMap API(external)

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 3000;
 
+const API_KEY = process.env.API_KEY;
+
+app.use(express.json());
+
+app.get('/', async (req,res) => {
+  const {city} = req.query;
+  console.log(city)
+  const base_url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+
+  try {
+    const myResponse = await axios.get(base_url);
+    const data = myResponse.data.main;
+    res.json({data});
+  } catch (error) {
+    console.log(error)
+    res.json(error);
+  }
+})
+
+app.listen(PORT, () => console.log(`listening on Port ${PORT}`))
